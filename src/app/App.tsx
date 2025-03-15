411c8c7e9f1b08e064d645bc2b36ac34bf4815ad
DeOwl <47011746+DeOwl@users.noreply.github.com>
2025-03-15 13:40:38 +0300

    basic_box_imported_style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CommentsPage from "../components/pages/CommentsPage/CommentsPage";
+import { ConfigProvider, theme } from 'antd';
+import './App.css';
+  
 
 const App: React.FC = () => {
   return (
     <Router>
+      <ConfigProvider
+        theme={{
+          algorithm: theme.defaultAlgorithm,
+        }}
+      >
       <Routes>
         <Route path="/" element={<CommentsPage />} />
       </Routes>
+      </ConfigProvider>
     </Router>
   );
 };
--- src/components/pages/CommentsPage/CommentsPage.tsx
+++ src/components/pages/CommentsPage/CommentsPage.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { DatePicker } from "antd";
 import styles from "./styles.module.scss";
+import DialogBox from "../../widgets/dialog_box/dialog_box";
 
 const CommentsPage: React.FC = () => {
   return (
     <div className={styles["comments-page"]}>
       <h1>Календарик</h1>
-      <DatePicker />
+      <DialogBox />
     </div>
   );
 };
