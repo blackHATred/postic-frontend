827fb702259a4efd04aeac4c2995579aeca8a657
karamel <krisdinabui@gmail.com>
2025-03-15 11:31:29 +0300

    init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CommentsPage from "../components/pages/CommentsPage/CommentsPage";
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
@@ -10,6 +10,6 @@ function App() {
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
